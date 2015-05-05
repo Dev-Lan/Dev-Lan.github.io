df663123944b48e66da832de29585b276a5a7fee
Devin Lange <lange604@umn.edu>
2015-05-04 23:36:31 -0500

    good enough draft

--- JournalismProject/ViralAlert.js
+++ JournalismProject/ViralAlert.js
@@ -13,15 +13,19 @@ var totalTime;
 var timeElapsed;
 var stepper; // used to end doStep
 var tableRows; // used to update table
-var gameOver;
+var gameInProgress;
+
+var MIN_SIZE;
+var MAX_SIZE;
+var AVE_PERCENT_INCREASE_PER_HOUR;
+var incrementAmount;
 
 function initializeData() {
 	percentDone = "0.0";
-	peopleExposed = 1;
+	peopleExposed = 0;
 	sites = ["facebook", "twitter", "reddit", "imgur", "instagram", "tumblr", "4chan", "google+", "youtube"];
 	users = [1415000000, 288000000, 100000000, 100000000, 300000000, 230000000, 18000000, 300000000, 1000000000];
-	peopleExposed = 1;
-	exposedUsers = [1,0,0,0,0,0,0,0,0];
+	exposedUsers = [0,0,0,0,0,0,0,0,0];
 	// user stats taken from the following sites
 	// http://www.statista.com/statistics/272014/global-social-networks-ranked-by-number-of-users/
 	// http://www.quora.com/How-many-registered-users-does-Reddit-currently-have
@@ -30,13 +34,33 @@ function initializeData() {
 	// http://www.quora.com/How-many-active-users-does-4chan-have
 	totalNumberOfInternetUsers = 3000000000;
 	//http://www.internetlivestats.com/internet-users/
-	gameOver = false;
+	gameInProgress = false; // 
 	timestep = 200; // in milliseconds
 	totalTime = 48000; // 48 hours at one second per hour
 	timeElapsed = 0;
 	tableRows = [];
+	MIN_SIZE = 2;
+	MAX_SIZE = 10;
+	AVE_PERCENT_INCREASE_PER_HOUR = 8;
+	incrementAmount = 1;
 
 }
+
+function tutorial() {
+	var tutorialDiv = document.createElement("div");
+	tutorialDiv.setAttribute("class", "overlayDiv");
+	tutorialDiv.setAttribute("id", "tutorialDiv");
+	var tutorialImg = document.createElement("img");
+	tutorialImg.setAttribute("src", "img/tutorial.png");
+	tutorialDiv.appendChild(tutorialImg);
+	var gotItButton = document.createElement("button");
+	gotItButton.setAttribute("class","gameUI");
+	gotItButton.setAttribute("onclick", "finishTutorial()");
+	gotItButton.innerText = "Got It!";
+	tutorialDiv.appendChild(gotItButton);
+	gameHolderDiv.appendChild(tutorialDiv);
+}
+
 
 function initializeDom() {
 	//progess bar
@@ -52,7 +76,7 @@ function initializeDom() {
 
     // info section
     var infoDiv = document.createElement("div");
-    infoDiv.setAttribute("class", "information");
+    infoDiv.setAttribute("class", "information fancyText");
     infoDiv.setAttribute("id", "aside");
     var heading = document.createElement("h2");
     heading.innerText = "Have you heard about \""+viralName+"\"?";
@@ -102,6 +126,8 @@ function initializeDom() {
     };
 
     var finalRow = document.createElement('tr');
+    finalRow.setAttribute("id", "finalTableRow");
+
     var site = document.createElement('td');
     site.innerText = "All internet users";
     finalRow.appendChild(site);
@@ -153,15 +179,21 @@ function initializeDom() {
 
 	  var node = svg.selectAll(".node")
 	      .data(graph.nodes)
-	    .enter().append("circle")
+	    .enter().append("g")
 	      .attr("class", "node")
-	      .attr("r", function(d) {return (d.size / 1415)*30 + 20; })
+	      // .attr("transform", function(d) {return "scale(" + ((d.size/1415)*3 + 1.1 ) +")"; })
 	      .attr("id",function(d) {return d.name;})
-	      .style("fill", function(d) { return '#000000ff'; })
+	      // .attr("onclick", function(d) { return increaseCount(d.index); })
+	      // .style("fill", function(d) { return '#000000ff'; })
 	      .call(force.drag);
 
 	  node.append("title")
 	      .text(function(d) { return d.name; });
+
+	  node.append("use")
+      .attr("xlink:href", function(d) {return "img/website-logos.svg#"+d.name ;} );
+
+      node.on('click', function(d) { return increaseCount(d.index);});
 
 	  force.on("tick", function() {
 	    link.attr("x1", function(d) { return d.source.x; })
@@ -169,28 +201,54 @@ function initializeDom() {
 	        .attr("x2", function(d) { return d.target.x; })
 	        .attr("y2", function(d) { return d.target.y; });
 
-	    node.attr("cx", function(d) { return d.x; })
-	        .attr("cy", function(d) { return d.y; });
+	    node.attr("transform", function(d) {
+	    	var scaleAmt = (exposedUsers[d.index] / 1415000000) * (MAX_SIZE - MIN_SIZE) + MIN_SIZE;
+	    	return "scale("+ scaleAmt +") translate(" + d.x / scaleAmt + "," + d.y / scaleAmt + ")"; })
 	  });
 	});
 
 
 }
 
+function increaseCount(index) {
+	if (exposedUsers[index] < users[index] && gameInProgress) {
+		exposedUsers[index] += incrementAmount;
+	}
+	return;
+}
+
 function updateSimulation() {
 	timeElapsed += timestep;
 	percentDone = 100*(timeElapsed / totalTime);
-	// should use time.
+
 	for (var i = 0; i < users.length; i++) {
-		exposedUsers[i] += 100;
+		var increase = exposedUsers[i]*0.00005*AVE_PERCENT_INCREASE_PER_HOUR*timestep; // todo add randomness
+		exposedUsers[i] += Math.round(increase);
+		exposedUsers[i] = Math.min(users[i], exposedUsers[i]);
 	};
 
+	
+	var mostExposedIndex = exposedUsers.indexOf(Math.max.apply(Math, exposedUsers));
+	peopleExposed = 0;
+	for (var i = 0; i < exposedUsers.length; i++) {
+		if (i == mostExposedIndex) {
+			peopleExposed += exposedUsers[i];
+		} else {
+			peopleExposed += (1585 / 2336) * exposedUsers[i];
+		}
+	};
+	peopleExposed = Math.min(totalNumberOfInternetUsers, Math.round(peopleExposed));
+
+
+	incrementAmount = Math.max(1, Math.round(0.01*peopleExposed));
+
 }
+
 
 function updateDom () {
 	document.getElementById("progressBar").style.width = percentDone + "%";
 	for (var i = 0; i < tableRows.length; i++) {
-		children = tableRows[i].children;
+		var children = tableRows[i].children;
 		children[1].innerText = exposedUsers[i].toLocaleString(); // exposed
 		children[2].innerText = (users[i]-exposedUsers[i]).toLocaleString(); // ignorant
 		
@@ -198,6 +256,12 @@ function updateDom () {
     	children[3].innerText = percentage.toPrecision(4) + "%"; // percent exposed
 	};
 	// update final row
+	var finalRow = document.getElementById("finalTableRow");
+	var finalChildren = finalRow.children;
+	finalChildren[1].innerText = peopleExposed.toLocaleString(); // total exposed
+	finalChildren[2].innerText = (totalNumberOfInternetUsers - peopleExposed).toLocaleString(); // ignorant
+	var percentage = (peopleExposed / totalNumberOfInternetUsers)*100;
+	finalChildren[3].innerText = percentage.toPrecision(4) + "%"; 
 
 }
 
@@ -206,13 +270,83 @@ function doStep() {
 	updateDom();
 	if (percentDone >= 100) {
 		window.clearInterval(stepper);
-		// clean up.
+		gameInProgress = false;
+		gameOver();
 	}
+}
+function getStatus() {
+	if(peopleExposed == 0) {
+			return "You have to click the graph!";
+	} else if (peopleExposed < 100) {
+			return "I guess you have a couple Facebook friends";
+	} else if (peopleExposed < 1000) {
+			return  "Looks like you have a LOT of Facebook friends. congrats.";
+	} else if (peopleExposed < 10000) {
+			return viralName +" has earned you microfame! Emphasis on micro...";
+	} else if (peopleExposed < 100000) {
+			return "";
+	} else if (peopleExposed < 1000000) {
+			return "";
+	} else if (peopleExposed < 10000000) {
+			return "";
+	} else if (peopleExposed < 100000000) {
+			return "";
+	} else if (peopleExposed < 1000000000) {
+			return "";
+	} else if (peopleExposed < 2000000000) {
+			return "";
+	} else if (peopleExposed == 3000000000) {
+			return viralName + " is the new Gangnam Style! Neato!";
+	}
+
+	return "";
+}
+
+function removeGameOver() { gameHolderDiv.removeChild(document.getElementById("gameOverDiv")); }
+
+function gameOver() {
+	var gameOverDiv = document.createElement("div");
+	gameOverDiv.setAttribute("class", "overlayDiv fancyText");
+	gameOverDiv.setAttribute("id", "gameOverDiv");
+	var h1 = document.createElement("h1");
+	h1.innerText = "Game Over!";
+	gameOverDiv.appendChild(h1);
+	var h2 = document.createElement("h2");
+	h2.innerText = getStatus();
+	gameOverDiv.appendChild(h2);
+	var newGameForm = document.createElement("form");
+	
+	var newGameButton = document.createElement("input");
+	newGameButton.setAttribute("type","submit");
+	newGameButton.setAttribute("value", "New Game");
+	newGameButton.setAttribute("class","gameUI")
+	newGameForm.appendChild(newGameButton);
+	
+	var playWithGraphThingy = document.createElement("button");
+	playWithGraphThingy.setAttribute("class","gameUI")
+	playWithGraphThingy.innerText = "Play with social graph thingy.";
+	playWithGraphThingy.setAttribute("onclick","removeGameOver()");
+	
+
+	gameOverDiv.appendChild(newGameForm);
+	gameOverDiv.appendChild(document.createElement("br"));
+	gameOverDiv.appendChild(playWithGraphThingy);
+
+	gameHolderDiv.appendChild(gameOverDiv);
 }
 
 function beginGameplay() {
+
 	initializeData();
 	initializeDom();
+	tutorial();
+}
+
+function finishTutorial() {
+	// this is where game play actually start. good function naming Devin...
+	var tutorialDiv = document.getElementById("tutorialDiv");
+	gameHolderDiv.removeChild(tutorialDiv);
+	gameInProgress = true;
 	stepper = setInterval(doStep, timestep);
 }
 
@@ -241,6 +375,7 @@ function createInitForm() {
 	var formHolder = document.createElement("div");
 	formHolder.setAttribute("class", "initForm");
 	formHolder.setAttribute("id", "initFormDiv");
+	formHolder.setAttribute("class", "fancyText");
 
 
 	//form
@@ -248,7 +383,7 @@ function createInitForm() {
 	form.setAttribute("id","initForm");
 	// viral name
 	var nameHeading = document.createElement("h2");
-	nameHeading.innerText = "Choose a name for your sensation!";
+	nameHeading.innerText = "Choose a name.";
 
 	var nameInput = document.createElement("input");
 	nameInput.setAttribute("type", "text");
@@ -297,31 +432,33 @@ function createInitForm() {
 	// submit
 	var submitButton = document.createElement("input");
 	submitButton.setAttribute("type","submit");
-	// submitButton.setAttribute("onClick", "initFormSubmit()");
+	submitButton.setAttribute("class", "gameUI");
 
 	// append to form
 	form.appendChild(nameHeading);
 	form.appendChild(nameInput);
 	form.appendChild(document.createElement("br"));
 	form.appendChild(document.createElement("br"));
-	form.appendChild(mediaTypeHeading);
-	form.appendChild(radioInput1);
-	form.appendChild(buttonLabel1);
-	form.appendChild(document.createElement("br"));
-	form.appendChild(radioInput2);
-	form.appendChild(buttonLabel2);
-	form.appendChild(document.createElement("br"));
-	form.appendChild(radioInput3);
-	form.appendChild(buttonLabel3);
-	form.appendChild(document.createElement("br"));
-	form.appendChild(radioInput4);
-	form.appendChild(buttonLabel4);
-	form.appendChild(document.createElement("br"));
-	form.appendChild(document.createElement("br"));
+	// Decided to omit media type, but left construction of it in.
+	// form.appendChild(mediaTypeHeading);
+	// form.appendChild(radioInput1);
+	// form.appendChild(buttonLabel1);
+	// form.appendChild(document.createElement("br"));
+	// form.appendChild(radioInput2);
+	// form.appendChild(buttonLabel2);
+	// form.appendChild(document.createElement("br"));
+	// form.appendChild(radioInput3);
+	// form.appendChild(buttonLabel3);
+	// form.appendChild(document.createElement("br"));
+	// form.appendChild(radioInput4);
+	// form.appendChild(buttonLabel4);
+	// form.appendChild(document.createElement("br"));
+	// form.appendChild(document.createElement("br"));
 	form.appendChild(submitButton);
 
 	var title = document.createElement("h1");
 	title.innerText = "Customize your very own Viral Sensation!";
+	title.setAttribute("class", "fancyText");
 	formHolder.appendChild(title);
 	formHolder.appendChild(form);
 
@@ -348,3 +485,4 @@ function start() {
 }
 
 window.addEventListener( "load", start, false );
+// 
