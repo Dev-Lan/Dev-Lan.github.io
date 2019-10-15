7f8450b2ef82318958503138aaa44d49b951186a
Devin Lange <devlan@Aule.local>
2019-10-14 21:18:30 -0600

    add 2 files that include depth calculation

---
+++ research/DataModel/CurveList.ts
@@ -0,0 +1,104 @@
+import { DevlibMath } from '../lib/DevlibMath';
+import { DevlibAlgo } from '../lib/DevlibAlgo';
+import { CurveND } from './CurveND';
+import { PointND } from './PointND';
+
+export class CurveList
+{
+
+	constructor(curveList: CurveND[])
+	{
+		this._curveList = curveList;
+	}
+
+	private _curveList : CurveND[];
+	public get curveList() : CurveND[] {
+		return this._curveList;
+	}
+
+	private _inputKey : string;
+	public get inputKey() : string {
+		return this._inputKey;
+	}
+
+	private initValue(key: string, value: number): void
+	{
+		for (let curve of this.curveList)
+		{
+			curve.set(key, value);
+		}
+	}
+
+	public setInputKey(key: string): void
+	{
+		this._inputKey = key;
+		for (let curve of this.curveList)
+		{
+			curve.sort(key);
+		}
+	}
+
+	public sort(key: string, ascend: boolean = true): void
+	{
+		let sortFunction = DevlibAlgo.sortOnProperty<CurveND>((curve: CurveND) => 
+		{
+			return curve.get(key);
+		}, ascend);
+		this.curveList.sort(sortFunction);
+	}
+
+	public calculateDepth(depthKey: string, valueKey: string): void
+	{
+		this.initValue(depthKey, 0);
+
+		const allBands = CurveList.getAllPossible2Bands(this.curveList) as [CurveND, CurveND][];
+		for (let band of allBands)
+		{
+			for (let curve of this.curveList)
+			{
+				const depthContribution = this.getDepthContribution(curve, band, valueKey);
+				const oldDepth = curve.get(depthKey);
+				curve.set(depthKey, oldDepth + depthContribution);
+			}
+		}
+		// todo - normalize
+
+	}
+
+	private getDepthContribution(curve: CurveND, [b1, b2]: [CurveND, CurveND], valueKey: string): number
+	{
+		let depth = 0;
+		// for (let point of curve.pointArray)
+		for (let i = 0; i < curve.pointArray.length; i++)
+		{
+			let point: PointND = curve.pointArray[i];
+			const t = point.get(this.inputKey);
+			let thisVal = point.get(valueKey);
+			let b1Val = b1.getPointValue(t, valueKey);
+			let b2Val = b2.getPointValue(t, valueKey);
+			let minVal = Math.min(b1Val, b2Val);
+			let maxVal = Math.max(b1Val, b2Val);
+			if (minVal <= thisVal && thisVal <= maxVal)
+			{
+				const weight = curve.getPointWeight(i);
+				depth += weight;
+			}
+		}
+		return depth;
+	}
+
+	static getAllPossible2Bands(list: any[]): [any, any][]
+	{
+		const bandList: [any, any][] = [];
+		for (let i = 0; i < list.length; i++)
+		{
+			for (let j = i + 1; j < list.length; j++)
+			{
+				let b: [any, any] = [list[i], list[j]];
+				bandList.push(b);
+			}
+		}
+		return bandList
+	}
+
+}
